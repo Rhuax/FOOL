6bd455746542d0a979c7ca1c7a71120058b16f8c
frasuri94 <fra.suri94@gmail.com>
2017-06-19 16:33:29 +0200

    asas

--- src/ast/FoolVisitorImpl.java
+++ src/ast/FoolVisitorImpl.java
@@ -314,7 +314,7 @@ public class FoolVisitorImpl extends FoolProvaBisBaseVisitor<Node> {
 		ArrayList<Node> methList = new ArrayList<Node>();
 
 		for(VardecContext vardec : ctx.vardec())
-			attList.add(visit(vardec.type()));
+			attList.add(visit(vardec));
 
 		for(FunContext fundec : ctx.fun())
 			methList.add(visit(fundec));
