e565125aa74204a76686cff574675d42f39f7bf5
crow <gelidcrow@gmail.com>
2017-07-11 10:35:00 +0200

    Aggiunto commentino

--- src/util/DispatchMethodTable.java
+++ src/util/DispatchMethodTable.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
  * Created by crow on 30/06/17.
  */
 public class DispatchMethodTable {
-    public HashMap<String, String> methodList;
+    public HashMap<String, String> methodList;//<Nome metodo, label>
 
     public DispatchMethodTable() {
         methodList = new HashMap<>();
